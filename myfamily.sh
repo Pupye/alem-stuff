04ab13249ce5bac2b1208da6f73522794dd13108
almus <Almas>
2019-11-12 16:01:54 +0600

    oh boy commited

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 token=$(curl -s https://01.alem.school/assets/superhero/all.json)
 
-echo $token | jq '.[0].connections.relatives' | sed 's/"//g'
+echo $token | jq '.[] | select(.id=='$HERO_ID') | .connections.relatives' | sed 's/"//g'
