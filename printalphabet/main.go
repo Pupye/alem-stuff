4ee1bb12141ef63b340b4a205b55d6c925380a91
Pupye <fullhouseya89@gmail.com>
2019-11-13 17:47:17 +0600

    final

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var alph string = "abcdefghijklmnopqrstuvwxyz\n"
-	for i := 0; i < len(alph); i++ {
-		z01.PrintRune(rune(alph[i]))
+	var i rune = 97
+	for ; i < 123; i++ {
+		z01.PrintRune(i)
 	}
+	z01.PrintRune(10)
 }
