026670b019f32dfc284e22f0e7e4916715b92ee0
Pupye <fullhouseya89@gmail.com>
2019-11-13 17:28:19 +0600

    printRuned

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/01-edu/z01"
+
 func main() {
-	print("zyxwvutsrqponmlkjihgfedcba")
+	var i rune = 122
+	for ; i > 96; i-- {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune(10)
 }
