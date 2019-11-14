1ad5897d3a3893889a615befa51ec624a16d65b7
Pupye <fullhouseya89@gmail.com>
2019-11-14 15:26:08 +0600

    here is my commit

---
+++ basicatoi.go
@@ -0,0 +1,20 @@
+package piscine
+
+//BasicAtoi is function that converts string to integer
+func BasicAtoi(s string) int {
+	mutableOne := []rune(s)
+	strLen := 0
+	for s := range mutableOne {
+		strLen = s
+	}
+	strLen++ // now we got len
+	tens := 1
+	res := 0
+	for index := strLen - 1; index >= 0; index-- {
+		slicedInt := mutableOne[index] - 48
+		res += int(slicedInt) * tens
+		tens *= 10
+	}
+
+	return res
+}
