45f9124969e6c2884afed190784d9ce36d7e568e
Pupye <fullhouseya89@gmail.com>
2019-11-18 12:33:34 +0600

    here is my commit

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,17 +2,16 @@ package piscine
 
 //RecursiveFactorial is a function that calculates factorial recursively
 func RecursiveFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
 	if nb == 0 {
 		return 1
 	}
 
-	if nb < 0 {
+	if nb*RecursiveFactorial(nb-1) < 0 {
 		return 0
 	}
-	temp := RecursiveFactorial(nb - 1)
-	result := nb * temp
-	if result/nb != temp {
-		return 0
-	}
-	return result
+	return nb * RecursiveFactorial(nb-1)
 }
