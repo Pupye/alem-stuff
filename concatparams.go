e5373c57b78fa151c2522363fdeaac72be48368d
Pupye <fullhouseya89@gmail.com>
2019-11-21 17:07:04 +0600

    here is my commit

--- concatparams.go
+++ concatparams.go
@@ -3,8 +3,31 @@ package piscine
 //ConcatParams is a function
 func ConcatParams(args []string) string {
 	build := ""
-	for index := range args {
-		build += args[index] + "\n"
+
+	length := getLenConCat(args)
+
+	for index := 0; index < length; index++ {
+		if index == length-1 {
+			build += args[index]
+		} else {
+			build += args[index] + "\n"
+		}
 	}
+
 	return build
+
+}
+
+func getLenConCat(str []string) int {
+	length := 0
+	isEmpty := true
+	for index := range str {
+		isEmpty = false
+		length = index
+	}
+	if !isEmpty {
+		length++
+	}
+
+	return length
 }
