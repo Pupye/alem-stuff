769a141599321385b7f14682b98a5234f1866b44
Pupye <fullhouseya89@gmail.com>
2019-11-21 18:08:50 +0600

    here is my commit

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,22 +3,59 @@ package piscine
 //SplitWhiteSpaces is a function
 func SplitWhiteSpaces(str string) []string {
 	numOfWords := 0
+	encountered := false
 	for x := range str {
-		if str[x] == ' ' {
-			numOfWords++
+		isDelimeter := (str[x] == ' ') || (str[x] == '\t') || (str[x] == '\n')
+		if isDelimeter {
+			encountered = false
+			continue
+		} else {
+			if !encountered {
+				numOfWords++
+				encountered = true
+			}
+
 		}
 	}
-	result := make([]string, numOfWords)
 
+	result := make([]string, numOfWords)
 	tempStr := ""
 	index := 0
-	for x := range str {
-		tempStr += string(str[x])
-		if str[x] == ' ' {
+	strLen := getLenSplitSpace(str)
+
+	for x := 0; x < strLen; x++ {
+		isDelimeter := (str[x] == ' ') || (str[x] == '\t') || (str[x] == '\n')
+		if isDelimeter {
+			continue
+		} else {
+
+			for !isDelimeter {
+				tempStr += string(str[x])
+
+				x++
+				if x == strLen {
+					break
+				}
+				isDelimeter = (str[x] == ' ') || (str[x] == '\t') || (str[x] == '\n')
+			}
 			result[index] = tempStr
-			index++
 			tempStr = ""
+			index++
 		}
 	}
 	return result
+}
+
+func getLenSplitSpace(str string) int {
+	length := 0
+	isEmpty := true
+	for index := range str {
+		isEmpty = false
+		length = index
+	}
+	if !isEmpty {
+		length++
+	}
+
+	return length
 }
