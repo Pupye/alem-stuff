6bd0486601fb4c1a676c1cce664297d9c8c2a94d
Pupye <fullhouseya89@gmail.com>
2019-11-26 20:15:11 +0600

    here is my commit

---
+++ displayfile/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	argumets := os.Args[1:]
+	length := getLennDis(argumets)
+	if length < 1 {
+		fmt.Println("File name missing")
+	} else if length > 1 {
+		fmt.Println("Too many arguments")
+	} else {
+		content, err := ioutil.ReadFile(argumets[0])
+		if err != nil {
+			fmt.Printf("the mistake is : %v\n", err.Error())
+		} else {
+			fmt.Printf("%s", content)
+		}
+	}
+
+}
+
+func getLennDis(args []string) int {
+	isEmpty := true
+	length := 0
+	for index := range args {
+		length = index
+		isEmpty = false
+	}
+	if !isEmpty {
+		length++
+	}
+	return length
+}
