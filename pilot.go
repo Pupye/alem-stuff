03b2bf6fd167cf406601d8ba07afb5982c1af8df
Pupye <fullhouseya89@gmail.com>
2019-11-28 10:13:42 +0600

    pilot

---
+++ pilot.go
@@ -0,0 +1,12 @@
+package piscine
+
+//Pilot is a struct
+type Pilot struct {
+	Name     string
+	Life     float64
+	Age      int64
+	Aircraft string
+}
+
+//AIRCRAFT1 is a next struct
+var AIRCRAFT1 string
