c08716d08818b34012ce24140bdd57748ed8fda7
Pupye <fullhouseya89@gmail.com>
2019-11-28 08:24:42 +0600

    engi

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+//Enigma is a function
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	temp := *******c
+	*******c = ***a
+	temp2 := ****d
+	****d = temp
+	temp = *b
+	*b = temp2
+	***a = temp
+}
