754ee60d7412198c8e19a60fec5881969d171259
Pupye <fullhouseya89@gmail.com>
2019-11-28 21:49:08 +0600

    printnbr works

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,6 +1,8 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 //PrintNbrBase is a function
 func PrintNbrBase(nbr int, base string) {
@@ -19,15 +21,52 @@ func printStrForBase(str string) { //validated
 }
 
 func isValidBase(base string) bool {
+	length := getLenOfStrBase(base)
 
-	return false
+	if length < 2 {
+		return false
+	}
+	mutableOne := []rune(base)
+
+	for x := range mutableOne {
+		if mutableOne[x] == '+' || mutableOne[x] == '-' {
+			return false
+		}
+		for index := x + 1; index < length; index++ {
+
+			if mutableOne[x] == mutableOne[index] {
+
+				return false
+			}
+		}
+	}
+
+	return true
 }
 
-func convertNbrIntoBase(n int, base string) []rune {
-	// radix := getLenOfStrBase(base)
-	// requiredLength := requiredLengthToConvert(n, radix)
+func convertNbrIntoBase(n int, base string) { //validated
+	if n < 0 {
+		z01.PrintRune('-')
+		n = n * -1
+	}
+	radix := getLenOfStrBase(base)
+	requiredLength := requiredLengthToConvert(n, radix)
+	count := requiredLength - 1
+	baseDigits := []rune(base)
+	for count >= 0 {
+		getReminderFrom := n / getPower(radix, count)
+		digit := getReminderFrom % radix
+		z01.PrintRune(baseDigits[digit])
+		count--
+	}
+}
 
-	return nil
+func getPower(nb int, power int) int {
+	result := 1
+	for index := 0; index < power; index++ {
+		result *= nb
+	}
+	return result
 }
 
 func requiredLengthToConvert(n int, b int) int { //validated
