d7efd93841e508b4637dec8b484dda3bcafc6e26
Pupye <fullhouseya89@gmail.com>
2019-11-28 14:14:15 +0600

    compacted

---
+++ compact.go
@@ -0,0 +1,21 @@
+package piscine
+
+//Compact this
+func Compact(ptr *[]string) int {
+	count := 0
+	for index := range *ptr {
+		if (*ptr)[index] == "" {
+			count++
+		}
+	}
+	compacted := make([]string, count)
+	compactIndex := 0
+	for index := range *ptr {
+		if (*ptr)[index] != "" {
+			compacted[compactIndex] = (*ptr)[index]
+			compactIndex++
+		}
+	}
+	*ptr = compacted
+	return count
+}
