e3476775497f16bf518c18734fb2d9a175286ba8
Pupye <fullhouseya89@gmail.com>
2019-11-28 11:18:07 +0600

    doopy

--- doop/main.go
+++ doop/main.go
@@ -1,51 +1,102 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	arguments := os.Args[1:]
 	length := getLenIsSortedFDoop(arguments)
 	if length != 3 {
-
 	} else {
-		argType := isValid(arguments[1])
-		if argType == -1 {
-			fmt.Print(0)
-			fmt.Print('\n')
-		} else if argType == 1 {
-			if arguments[0] == "0" && arguments[2] == "0" {
-				fmt.Print(0)
-				fmt.Print('\n')
-			} else if arguments[0] == "0" {
-
-				fmt.Print(atoiForDoop(arguments[2])) // handle error
-				fmt.Print('\n')
-			} else if arguments[2] == "0" {
-				fmt.Print(atoiForDoop(arguments[2])) //handle error
-				fmt.Print('\n')
+		operatorValid := isValid(arguments[1])
+		argValid := isValidArguments(arguments[0], arguments[2])
+		if operatorValid == -1 || !argValid {
+			z01.PrintRune('0')
+			z01.PrintRune('\n')
+		} else if operatorValid == 1 {
+			//here we sure that args are valid
+			n1, isOverFlow1 := atoiForDoop(arguments[0])
+			n2, isOverFlow2 := atoiForDoop(arguments[2])
+			if isOverFlow1 || isOverFlow2 {
 			} else {
 
+				if operationOverFlow(n1, n2, 1) {
+
+				} else {
+
+				}
 			}
+		} else if operatorValid == 2 {
+			n1, isOverFlow1 := atoiForDoop(arguments[0])
+			n2, isOverFlow2 := atoiForDoop(arguments[2])
+			if isOverFlow1 || isOverFlow2 {
+			} else {
 
-		} else if argType == 2 {
+				if operationOverFlow(n1, n2, 2) {
 
-		} else if argType == 3 {
+				} else {
 
-		} else if argType == 4 {
-			if arguments[2] == "0" {
-				fmt.Print("No division by 0")
-				fmt.Print('\n')
+				}
 			}
-		} else if argType == 5 {
+		} else if operatorValid == 3 {
+
+			n1, isOverFlow1 := atoiForDoop(arguments[0])
+			n2, isOverFlow2 := atoiForDoop(arguments[2])
+			if isOverFlow1 || isOverFlow2 {
+
+			} else {
+
+				if operationOverFlow(n1, n2, 3) {
+
+				} else {
+
+				}
+			}
+		} else if operatorValid == 4 {
 			if arguments[2] == "0" {
-				fmt.Print("No Modulo by 0")
-				fmt.Print('\n')
+				z01.PrintRune('k')
+			} else {
+				n1, isOverFlow1 := atoiForDoop(arguments[0])
+				n2, isOverFlow2 := atoiForDoop(arguments[2])
+				if isOverFlow1 || isOverFlow2 {
+
+				} else {
+
+					if operationOverFlow(n1, n2, 4) {
+
+					} else {
+
+					}
+				}
+			}
+		} else if operatorValid == 5 {
+			if arguments[2] == "0" {
+				z01.PrintRune('k')
+			}
+			n1, isOverFlow1 := atoiForDoop(arguments[0])
+			n2, isOverFlow2 := atoiForDoop(arguments[2])
+			if isOverFlow1 || isOverFlow2 {
+
+			} else {
+
+				if operationOverFlow(n1, n2, 1) {
+
+				} else {
+
+				}
 			}
 		}
 	}
+}
+
+func isValidArguments(n1, n2 string) bool {
+	if ssNumeric(n1) && ssNumeric(n2) {
+		return true
+	}
+	return false
 }
 
 func isValid(a string) int {
@@ -76,9 +127,9 @@ func getLenIsSortedFDoop(tab []string) int {
 	return length
 }
 
-func atoiForDoop(s string) (int, int) {
+func atoiForDoop(s string) (int64, bool) {
 	if s == "" {
-		return 0, 0
+		return 0, false
 	}
 	mutableOne := []rune(s)
 	strLen := 0
@@ -87,20 +138,121 @@ func atoiForDoop(s string) (int, int) {
 	}
 	strLen++ // now we got len
 	tens := 1
-	res := 0
+	var res int64
+	res = 0
 	for index := strLen - 1; index >= 0; index-- {
-		if mutableOne[index] == 45 && index == 0 {
-			return res * (-1), 0
-		} else if mutableOne[index] == 43 && index == 0 {
-			return res, 0
+		if index == 0 && mutableOne[index] == 45 {
+			return res, false
 		}
 		if mutableOne[index] < 48 || mutableOne[index] > 57 {
-			return 0, 0
+			return 0, false
 		}
-		slicedInt := mutableOne[index] - 48
-		res += int(slicedInt) * tens
-		tens *= 10
+
+		if mutableOne[0] == 45 {
+			slicedInt := mutableOne[index] - 48
+			res -= int64(int(slicedInt) * tens)
+
+			if res/int64(tens) != int64(slicedInt)*-1 {
+				return 0, true
+			}
+
+			tens *= 10
+		} else {
+			slicedInt := mutableOne[index] - 48
+			res += int64(int(slicedInt) * tens)
+			if res/int64(tens) != int64(slicedInt) {
+				return 0, true
+			}
+			tens *= 10
+		}
+
 	}
 
-	return res, 0
+	return res, false
+}
+
+func ssNumeric(str string) bool {
+	if str == "" {
+		return false
+	}
+
+	mutableOne := []rune(str)
+	for index := 0; index < getLenIsIsIs(str); index++ {
+		if mutableOne[0] == 45 && index == 0 {
+			continue
+		}
+		isNumeric := mutableOne[index] > 47 && mutableOne[index] < 58
+		if !(isNumeric) {
+			return false
+		}
+	}
+	return true
+}
+
+func getLenIsIsIs(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	length := 0
+	runes := []rune(s)
+	for x := range runes {
+		length = x
+	}
+	length++
+
+	return length
+}
+
+func operationOverFlow(n1 int64, n2 int64, argType int) bool {
+	var intMax int64
+
+	intMax = 9223372036854775807
+
+	if argType == 1 {
+		if n1 < 0 && n2 < 0 {
+			if n1+n2 >= 0 {
+				return true
+			}
+		}
+		if n2 >= 0 {
+			if n1 > intMax-n2 {
+				return true
+			}
+		} else {
+			if n1 < intMax-n2 {
+
+				return true
+			}
+		}
+	} else if argType == 2 {
+		if n1 > 0 && n2 > 0 {
+			return false
+		}
+
+		if n1 < 0 && n2 < 0 {
+			return false
+		}
+
+		if n1 < 0 {
+			if n1-n2 > 0 {
+				return true
+			}
+		} else if n2 < 0 {
+			if n1-n2 < 0 {
+				return true
+			}
+		}
+
+	} else if argType == 3 {
+		res := n1 * n2
+		if res != 0 && res/n1 != n2 {
+			return true
+		}
+	} else if argType == 4 {
+		return false
+	} else if argType == 5 {
+		return false
+	}
+	return false
 }
