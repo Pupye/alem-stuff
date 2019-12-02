5112d9510a0041182b0d965331be20ce81616918
Pupye <fullhouseya89@gmail.com>
2019-12-02 14:43:42 +0600

    here is my commit

--- listlast.go
+++ listlast.go
@@ -1,11 +1,5 @@
 package piscine
 
-//List is a struct
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 //ListLast is a function that returns last element of the list
 func ListLast(l *List) interface{} {
 	if l.Head == nil {
