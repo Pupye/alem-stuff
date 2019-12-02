c5907f09c14a2302e89f38842ec8d8782b4f259d
Pupye <fullhouseya89@gmail.com>
2019-12-02 13:33:00 +0600

    here is my commit

--- listpushback.go
+++ listpushback.go
@@ -15,11 +15,14 @@ type List struct {
 //ListPushBack is a function to push back the data
 func ListPushBack(l *List, data interface{}) {
 	n := &NodeL{Data: data}
-	if l.Tail == nil {
+	if l.Head == nil {
 		l.Head = n
-		l.Tail = n
 	} else {
-		l.Tail.Next = n
-		l.Tail = l.Tail.Next
+		pointerToCurrent := l.Head
+		for pointerToCurrent.Next != nil {
+			pointerToCurrent = pointerToCurrent.Next
+		}
+		pointerToCurrent.Next = n
 	}
+
 }
