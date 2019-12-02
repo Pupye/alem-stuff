022e4bf4450350651baa8ff7413d08819eb41037
Pupye <fullhouseya89@gmail.com>
2019-12-02 13:43:13 +0600

    here is my commit

--- listpushback.go
+++ listpushback.go
@@ -1,17 +1,5 @@
 package piscine
 
-//NodeL is a struct
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-//List is a struct
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 //ListPushBack is a function to push back the data
 func ListPushBack(l *List, data interface{}) {
 	n := &NodeL{Data: data}
