fa776a5b0f7fce49cf86f1899bd89b9b95cbded5
Pupye <fullhouseya89@gmail.com>
2019-12-02 13:49:28 +0600

    here is my commit

--- listpushfront.go
+++ listpushfront.go
@@ -1,17 +1,5 @@
 package piscine
 
-//NodeL is a struct
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-//List is a struct
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 //ListPushFront pushes to the front
 func ListPushFront(l *List, data interface{}) {
 	n := &NodeL{Data: data}
