812c390907c78480d7185d98f8fe7b2c5465061f
Pupye <fullhouseya89@gmail.com>
2019-12-03 18:55:36 +0600

    here is my commit

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -5,11 +5,11 @@ func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	f(root.Data)
 
 	if root.Left != nil {
-		BTreeApplyInorder(root.Left, f)
+		BTreeApplyPreorder(root.Left, f)
 	}
 
 	if root.Right != nil {
-		BTreeApplyInorder(root.Right, f)
+		BTreeApplyPreorder(root.Right, f)
 	}
 
 }
