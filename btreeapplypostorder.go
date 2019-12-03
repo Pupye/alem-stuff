958519812473bf9f8e30fb94977ef4e62c8bcf14
Pupye <fullhouseya89@gmail.com>
2019-12-03 18:07:54 +0600

    here is my commit

---
+++ btreeapplypostorder.go
@@ -0,0 +1,14 @@
+package piscine
+
+//BTreeApplyPostorder ...
+func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root.Left != nil {
+		BTreeApplyInorder(root.Left, f)
+	}
+
+	if root.Right != nil {
+		BTreeApplyInorder(root.Right, f)
+	}
+
+	f(root.Data)
+}
