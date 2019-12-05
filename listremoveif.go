269802df2f5ea75a0480cba949757af2f2016f84
Pupye <fullhouseya89@gmail.com>
2019-12-05 18:46:44 +0600

    here is my commit

---
+++ listremoveif.go
@@ -0,0 +1,26 @@
+package piscine
+
+//ListRemoveIf is a function
+func ListRemoveIf(l *List, data_ref interface{}) {
+	if l.Head == nil {
+		return
+	}
+
+	if l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
+
+	current := l.Head
+
+	for current.Next != nil {
+		if current.Next.Data == data_ref {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+
+	if current.Data == data_ref { //takes care of last node
+		current = nil
+	}
+
+}
