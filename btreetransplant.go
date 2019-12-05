1e29ce119f49c929e97df65487f977f0468a4642
Pupye <fullhouseya89@gmail.com>
2019-12-05 18:35:16 +0600

    here is my commit

--- btreetransplant.go
+++ btreetransplant.go
@@ -2,18 +2,24 @@ package piscine
 
 //BTreeTransplant is a function good ..
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	if node.Parent == nil {
+	if node.Parent == nil { // node here is root
 		root = rplc
 		return root
 	}
-	if node.Parent.Left == node {
+
+	if node.Parent.Left == node { //node is left child of its parent
 		node.Parent.Left = rplc
-		rplc.Parent = node.Parent
+		if rplc != nil {
+			rplc.Parent = node.Parent
+		}
 		return root
 	}
+
 	if node.Parent.Right == node {
 		node.Parent.Right = rplc
-		rplc.Parent = node.Parent
+		if rplc != nil {
+			rplc.Parent = node.Parent
+		}
 		return root
 	}
 
