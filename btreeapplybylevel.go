9d5b3d786f81c3acd320342da3ed7d92336a23c5
Pupye <fullhouseya89@gmail.com>
2019-12-05 16:57:31 +0600

    here is my commit

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -2,14 +2,24 @@ package piscine
 
 //BTreeApplyByLevel ...
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
-	f(root.Data)
-
-	if root.Left != nil {
-		BTreeApplyByLevel(root.Left, f)
+	h := BTreeLevelCount(root)
+	for index := 1; index <= h; index++ {
+		helper(root, f, index)
 	}
-
-	if root.Right != nil {
-		BTreeApplyByLevel(root.Right, f)
-	}
-
 }
+
+func helper(node *TreeNode, f func(...interface{}) (int, error), level int) {
+	if node == nil {
+		return
+	}
+	if level == 1 {
+		f(node.Data)
+	} else if level > 1 {
+		helper(node.Left, f, level-1)
+		helper(node.Right, f, level-1)
+	}
+}
+
+//so there is we need to something
+
+//
